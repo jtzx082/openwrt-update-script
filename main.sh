31de5055a2df3e7e86c5017e38ae8368fb1baf77
perfume <yxll@qql.com>
2021-01-22 16:47:27 +0800

    version v1.3.0

--- main.sh
+++ main.sh
@@ -1,19 +1,22 @@
-#/usr/bin/bash
+#usr/bin/bash
 #当前的md5获取方式不怎么牢靠，有待优化
 echo ###############################################################
 echo Openwrt firmware one-click update compilation script		   #
 echo															   #
 echo script By Lenyu										       #
 echo 															   #
-echo version v1.2.1											       #
+echo version v1.3.0											       #
 echo #################################################################
 sleep 3
-clear
-cd ~/
+#获取当前脚本所在的目录
+path=$(dirname $(readlink -f $0))
+#调用
+cd ${path} >/dev/null 2>&1
+echo ${path}
 echo
 ##lede
-git -C ~/lede pull >/dev/null 2>&1
-git -C ~/lede rev-parse HEAD > new_lede
+git -C ${path}/lede pull >/dev/null 2>&1
+git -C ${path}/lede rev-parse HEAD > new_lede
 new_lede=`cat new_lede`
 #判断old_lede是否存在，不存在创建
 if [ ! -f "old_lede" ]; then
@@ -25,25 +28,25 @@ fi
 sleep 0.1
 old_lede=`cat old_lede`
 if [ "$new_lede" = "$old_lede" ]; then
-	echo "no_update" > /home/lenyu/nolede
+	echo "no_update" > ${path}/nolede
 else
-	echo "update" > /home/lenyu/nolede
+	echo "update" > ${path}/nolede
 	echo $new_lede > old_lede
 fi
 echo
 ##xray
 if [ ! -d  "xray_update" ]; then
 	echo "xray_update文件夹不存在，准备创建…"
-	mkdir -p ~/xray_update
+	mkdir -p ${path}/xray_update
 else
-	count=`ls /home/lenyu/xray_update`
+	count=`ls ${path}/xray_update`
 	if [ "$count" > "0" ]; then  #判断文件夹是否为0,否则git拉去xray源码
-		git -C ~/xray_update pull >/dev/null 2>&1
-		git -C ~/xray_update rev-parse HEAD > ~/new_xray
+		git -C ${path}/xray_update pull >/dev/null 2>&1
+		git -C ${path}/xray_update rev-parse HEAD > ${path}/new_xray
 	else
-		git clone https://github.com/XTLS/Xray-core.git ~/xray_update #后面指定目录
-		git -C ~/xray_update pull >/dev/null 2>&1
-		git -C ~/xray_update rev-parse HEAD > ~/new_xray
+		git clone https://github.com/XTLS/Xray-core.git ${path}/xray_update #后面指定目录
+		git -C ${path}/xray_update pull >/dev/null 2>&1
+		git -C ${path}/xray_update rev-parse HEAD > ${path}/new_xray
 	fi
 fi
 echo
@@ -60,20 +63,20 @@ old_xray=`cat old_xray`
 #有xray更新就替换最新的commit分支id
 if [ "$new_xray" = "$old_xray" ]; then
 	clear
-	echo "no_update" > /home/lenyu/noxray
+	echo "no_update" > ${path}/noxray
 else
 	clear
 	echo "发现更新，准备切换到最新的commit分支MD5"
-	echo "update" > /home/lenyu/noxray
+	echo "update" > ${path}/noxray
 	sleep 1
 	#替换最新的md5值 sed要使用""才会应用变量
-	sed -i "s/.*PKG_SOURCE_VERSION:.*/PKG_SOURCE_VERSION:=$new_xray/" /home/lenyu/lede/package/lean/xray/Makefile
+	sed -i "s/.*PKG_SOURCE_VERSION:.*/PKG_SOURCE_VERSION:=$new_xray/" ${path}/lede/package/lean/xray/Makefile
 	echo $new_xray > old_xray
 fi
 echo
 ##passwall
-git -C ~/lede/feeds/passwall pull >/dev/null 2>&1
-git -C ~/lede/feeds/passwall rev-parse HEAD > new_passw
+git -C ${path}/lede/feeds/passwall pull >/dev/null 2>&1
+git -C ${path}/lede/feeds/passwall rev-parse HEAD > new_passw
 new_passw=`cat new_passw`
 #判断old_passw是否存在，不存在创建
 if [ ! -f "old_passw" ]; then
@@ -85,15 +88,15 @@ fi
 sleep 0.1
 old_passw=`cat old_passw`
 if [ "$new_passw" = "$old_passw" ]; then
-	echo "no_update" > /home/lenyu/nopassw
+	echo "no_update" > ${path}/nopassw
 else
-	echo "update" > /home/lenyu/nopassw
+	echo "update" > ${path}/nopassw
 	echo $new_passw > old_passw
 fi
 echo
 ##ssr+
-git -C ~/lede/feeds/helloworld pull >/dev/null 2>&1
-git -C ~/lede/feeds/helloworld rev-parse HEAD > new_ssr
+git -C ${path}/lede/feeds/helloworld pull >/dev/null 2>&1
+git -C ${path}/lede/feeds/helloworld rev-parse HEAD > new_ssr
 new_ssr=`cat new_ssr`
 #判断old_ssr是否存在，不存在创建
 if [ ! -f "old_ssr" ]; then
@@ -105,15 +108,15 @@ fi
 sleep 0.1
 old_ssr=`cat old_ssr`
 if [ "$new_ssr" = "$old_ssr" ]; then
-	echo "no_update" > /home/lenyu/nossr
+	echo "no_update" > ${path}/nossr
 else
-	echo "update" > /home/lenyu/nossr
+	echo "update" > ${path}/nossr
 	echo $new_ssr > old_ssr
 fi
 echo
 ##openclash
-git -C ~/lede/package/luci-app-openclash  pull >/dev/null 2>&1
-git -C ~/lede/package/luci-app-openclash  rev-parse HEAD > new_clash
+git -C ${path}/lede/package/luci-app-openclash  pull >/dev/null 2>&1
+git -C ${path}/lede/package/luci-app-openclash  rev-parse HEAD > new_clash
 new_clash=`cat new_clash`
 #判断old_clash是否存在，不存在创建
 if [ ! -f "old_clash" ]; then
@@ -125,18 +128,21 @@ fi
 sleep 0.1
 old_clash=`cat old_clash`
 if [ "$new_clash" = "$old_clash" ]; then
-	echo "no_update" > /home/lenyu/noclash
+	echo "no_update" > ${path}/noclash
 else
-	echo "update" > /home/lenyu/noclash
+	echo "update" > ${path}/noclash
 	echo $new_clash > old_clash
 fi
 echo
+
+
+
 #总结判断之
-nolede=`cat /home/lenyu/nolede`
-noclash=`cat /home/lenyu/noclash`
-noxray=`cat /home/lenyu/noxray`
-nossr=`cat /home/lenyu/nossr`
-nopassw=`cat /home/lenyu/nopassw`
+nolede=`cat ${path}/nolede`
+noclash=`cat ${path}/noclash`
+noxray=`cat ${path}/noxray`
+nossr=`cat ${path}/nossr`
+nopassw=`cat ${path}/nopassw`
 sleep 0.5
 if [[("$nolede" = "update") || ("$noclash" = "update") || ("$noxray" = "update") || ("$nossr" = "update" ) || ("$nopassw"  = "update" )]]; then
 	clear
@@ -145,13 +151,13 @@ if [[("$nolede" = "update") || ("$noclash" = "update") || ("$noxray" = "update")
 	clear
 	echo
 	echo "准备开始编译最新固件…"
-	source /etc/environment && cd /home/lenyu/lede && ./scripts/feeds update -a  && ./scripts/feeds install -a && make defconfig && make -j8 download && make -j10 V=s &&  bash rename.sh
+	source /etc/environment && cd ${path}/lede && ./scripts/feeds update -a  && ./scripts/feeds install -a && make defconfig && make -j8 download && make -j10 V=s &&  bash rename.sh
 	echo
-	rm -rf /home/lenyu/noxray
-	rm -rf /home/lenyu/noclash
-	rm -rf /home/lenyu/nolede
-	rm -rf /home/lenyu/nossr
-	rm -rf /home/lenyu/nopassw
+	rm -rf ${path}/noxray
+	rm -rf ${path}/noclash
+	rm -rf ${path}/nolede
+	rm -rf ${path}/nossr
+	rm -rf ${path}/nopassw
 	echo "固件编译成功，脚本退出！"
 	echo
 	exit 0
@@ -163,11 +169,11 @@ if [[("$nolede" = "no_update") && ("$noclash" = "no_update") && ("$noxray" = "no
 	echo "呃呃…检查lede/ssr+/xray/passwall/openclash源码，没有一个源码更新哟…还是稍安勿躁…"
 fi
 #脚本结束，准备最后的清理工作
-rm -rf /home/lenyu/noxray
-rm -rf /home/lenyu/noclash
-rm -rf /home/lenyu/nolede
-rm -rf /home/lenyu/nossr
-rm -rf /home/lenyu/nopassw
+rm -rf ${path}/noxray
+rm -rf ${path}/noclash
+rm -rf ${path}/nolede
+rm -rf ${path}/nossr
+rm -rf ${path}/nopassw
 echo
 echo "脚本退出！"
 echo
