f977f68c45669e2d15c19193085ae792d68bc378
perfume <yxll@qql.com>
2021-01-22 20:59:41 +0800

    Fix bugs

--- main.sh
+++ main.sh
@@ -144,31 +144,31 @@ cat>${path}/lede/rename.sh<<EOF
 #/usr/bin/bash
 path=\$(dirname \$(readlink -f \$0))
 cd \${path}
-		rm -rf \${path}/bin/targets/x86/64/*Lenyu.img.gz
+		    rm -rf \${path}/bin/targets/x86/64/*Lenyu.img.gz
         rm -rf \${path}/bin/targets/x86/64/packages
         rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic.manifest
         rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-rootfs-squashfs.img.gz
         rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-combined-squashfs.vmdk
         rm -rf \${path}/bin/targets/x86/64/config.seed
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-uefi-gpt-squashfs.vmdk
+		    rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-uefi-gpt-squashfs.vmdk
         rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-vmlinuz
-		rm -rf \${path}/bin/targets/x86/64/sha256sums
-		rm -rf \${path}/bin/targets/x86/64/config.buildinfo
-		rm -rf \${path}/bin/targets/x86/64/feeds.buildinfo
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-kernel.bin
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.vmdk
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img.gz
-		rm -rf \${path}/bin/targets/x86/64/version.buildinfo
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img
-		rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img
+        rm -rf \${path}/bin/targets/x86/64/sha256sums
+        rm -rf \${path}/bin/targets/x86/64/config.buildinfo
+        rm -rf \${path}/bin/targets/x86/64/feeds.buildinfo
+        rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-kernel.bin
+        rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.vmdk
+        rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk
+        rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img.gz
+        rm -rf \${path}/bin/targets/x86/64/version.buildinfo
+        rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img
+        rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img
+        rm -rf \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img
         sleep 3
         stre=\`sed '11!d'  \${path}/include/kernel-version.mk\` >nul 2>nul
         sleep 2
         mv \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz      \${path}/bin/targets/x86/64/openwrt_x86-64-\`date '+%m%d'\`_5.4.\`echo \${stre#* .}\`_dev_Lenyu.img.gz
         mv \${path}/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz  \${path}/bin/targets/x86/64/openwrt_x86-64-\`date '+%m%d'\`_5.4.\`echo \${stre#* .}\`_uefi-gpt_dev_Lenyu.img.gz
-		exit 0
+		  exit 0
 EOF
 fi
 sleep 0.2
